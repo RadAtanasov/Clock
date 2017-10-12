c41e9d104789fc4bfc3a16ad891a51fd83cb766f
Radion <RadionAtanasovKharkiv@gmail.com>
2017-10-13 02:01:18 +0300

    Dynamics added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 var hourHand = document.getElementsByClassName('hour_hand')[0];
 var minuteHand = document.getElementsByClassName('minute_hand')[0];
 var secondHand = document.getElementsByClassName('second_hand')[0];
-setInterval(function(){
+//var startDate = new Date();
+//var startRotateSecondHand = startDate.getSeconds();
+//var startRotateMinuteHand = startDate.getMinutes();
+//var startRotateHourHand = startDate.getHours();
+//secondHand.style.transform = 'rotate'+'('+(startRotateSecondHand)+')';
+//minuteHand.style.transform = 'rotate'+'('+(startRotateMinuteHand)+')';
+//hourHand.style.transform = 'rotate'+'('+(startRotateHourHand)+')';
+var countTime = setInterval(function(){
   var date = new Date();
   var degRotateSecondHand = (360 * date.getSeconds()/60)+'deg';
   var degRotateMinuteHand = (360 * date.getMinutes()/60)+'deg';
   var degRotateHourHand = (360 * date.getHours()/12)+0.5*(360 * date.getMinutes()/360)+'deg';
-  console.log(degRotateHourHand);
   secondHand.style.transform = 'rotate'+'('+(degRotateSecondHand)+')';
   minuteHand.style.transform = 'rotate'+'('+(degRotateMinuteHand)+')';
   hourHand.style.transform = 'rotate'+'('+(degRotateHourHand)+')';
+  secondHand.style.display = 'block';
+  minuteHand.style.display = 'block';
+  hourHand.style.display = 'block';
 }, 200);
